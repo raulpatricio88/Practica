3c651807c713e7b4433930018d50ba43cf9990ef
raulpatricio88 <99994569+raulpatricio88@users.noreply.github.com>
2022-03-14 18:19:22 -0300

    Add files via upload
    Planilla de remuneracion basica hecha con Openpyxl

--- Producto.py
+++ Producto.py
@@ -24,8 +24,18 @@ sheet['B2'] = 'Nombre'
 sheet['B2'].border = border
 sheet['C2'] = 'Rut'
 sheet['C2'].border = border
-sheet['D2'] = 'Sueldo Bruto'
+sheet['D2'] = 'Cargo'
 sheet['D2'].border = border
+sheet['E2'] = 'Sueldo base'
+sheet['E2'].border = border
+sheet['F2'] = 'Fecha de ingreso'
+sheet['F2'].border = border
+sheet['G2'] = 'Jornada'
+sheet['G2'].border = border
+sheet['H2'] = 'Años de antiguedad'
+sheet['H2'].border = border
+sheet['J2'] = 'Gratificacion legal'
+sheet['J2'].border = border
 
 for i in range (3, 71):
     while (opcion=='s'):
@@ -39,9 +49,7 @@ for i in range (3, 71):
         cargo = input("Cargo: ")
         sheet[f'D{i}'] = cargo
         sheet[f'D{i}'].border = border
-        sueldoB = float(input("Sueldo bruto: "))
-        sheet[f'E{i}'] = sueldoB
-        sheet[f'E{i}'].border = border
+
         fechaIng = input("Fecha de ingreso: (formato dd-mm-aa) ")
         sheet[f'F{i}'] = fechaIng
         sheet[f'F{i}'].border = border
@@ -49,22 +57,26 @@ for i in range (3, 71):
         jornada = input("Tipo de jornada: c: completa / m: media ")
         if(jornada=='c'):
             jornada1='Completa'
-        jornada1='Media'
+        else:
+            jornada1='Media'
         sheet[f'G{i}'] = jornada1
         sheet[f'G{i}'].border = border
         antiwedad = input("Años de antiguedad: ")
         sheet[f'H{i}'] = antiwedad
         sheet[f'H{i}'].border = border
+        sueldoB = float(input("Sueldo base: "))
+        sheet[f'E{i}'] = sueldoB
+        sheet[f'E{i}'].border = border
+        grat = float(input("Gratificacion legal: $"))
+        sheet[f'J{i}'] = grat
+        sheet[f'J{i}'].border = border
 
-        sueldoN= sueldoB - (sueldoB)*0.19
+        sueldoN= sueldoB + grat - (sueldoB)*0.19
         print("El sueldo neto es: $", + float(sueldoN))
         i+=1
         opcion = input("¿Desea agregar otro colaborador? s: si / n: no \N{tab}")
 
-
-
-
-
+print("Documento generado satisfactoriamente. Que tenga un buen día!")
 book.save('Prueba_producto.xlsx')
 
 
